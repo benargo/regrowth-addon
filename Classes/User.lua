b75da6aace723b78a013301d6a748df1ef3adbc9
Ben Argo <benargo@users.noreply.github.com>
2026-07-25 13:54:38 +0100

    Remove canSendUpdates field and associated logic from User class initialization

--- Classes/User.lua
+++ Classes/User.lua
@@ -8,24 +8,10 @@ local _, Regrowth = ...;
 ---@field canSendUpdates boolean
 local User = {
     _initialized = false,
-    canSendUpdates = false,
 };
 
 ---@type User
 Regrowth.User = User;
-
-local function CanSendUpdates()
-    if Regrowth.User.name == "Khamira" or Regrowth.User.name == "Kyukon" then
-        return true;
-    end
-
-    if not Regrowth:isCurrentVersion() then
-        Regrowth:warning("Can't send Regrowth_Data - Version out of date.");
-        return false;
-    end
-
-    return C_GuildInfo.IsGuildOfficer();
-end
 
 function User:_init()
     if (self._initialized) then
@@ -35,9 +21,6 @@ function User:_init()
     self.name = UnitName("player");
     self.realm = GetRealmName():gsub("-", "");
     self.fqn = Regrowth:getFullyQualifiedName(self.name, self.realm);
-    self.canSendUpdates = CanSendUpdates();
-
-    Regrowth:debug("User config: You " .. (self.canSendUpdates and "CAN" or "CAN NOT") .. " receive messages.")
 
     self._initialized = true;
 end
